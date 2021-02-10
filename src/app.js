c534852bc6a1a90bff5c593e28da8f4d5bdc2bda
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2021-02-10 20:38:46 +0100

    1st try

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 const app = require('express')();
 const superRouter = require('./routes')
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+      .create({
+         from: 'whatsapp:+14155238886',
+         body: 'Hello there!',
+         to: 'whatsapp:+34652568088'
+       })
+      .then(message => console.log(message.sid));
+
 app.get('/', function(req, res) {
     res.status(200).send("Server is running")
 });
