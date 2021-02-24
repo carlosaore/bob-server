de202178490c3e20264ae219ad977c8a1d23ee43
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2021-02-24 10:58:38 +0100

    update

--- src/app.js
+++ src/app.js
@@ -10,23 +10,14 @@ const sendToFam = require('./routes/sendtofam');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-/*
 app.get('/', function(req, res) {
-    client.messages 
-      .create({
-         from: 'whatsapp:+14155238886',
-         body: 'I opened the first beer',
-         to: 'whatsapp:+34652568088'
-       })
-      .then(message => console.log(message.sid));
-    res.status(200).send("message sent")
+    console.log("access");
+    res.status(200).send("API running")
 });
-*/
-//https://www.twilio.com/blog/send-images-whatsapp-node-js
 
 app.use('/sendtochief', sendToChief);
 app.use('/sendtofam', sendToFam);
-/*
+
 app.post('/beer', (req, res) => {
     let quantity = Number(req.body.Body.replace(/\D/g,''));
     quantity = quantity < 10 ? quantity : 10;
@@ -38,37 +29,5 @@ app.post('/beer', (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
   });
-
-/*
-app.get('/', function(req, res) {
-    client.messages 
-      .create({
-         from: 'whatsapp:+14155238886',
-         body: 'sent you a beer from a get request',
-         to: 'whatsapp:+34652568088'
-       })
-      .then(message => console.log(message));
-    //res.status(200).send("message sent")
-    //const response = new MessagingResponse();
-   // response.message(`Beer`)
-    res.status(200).send("beer sent")
-});
-
-app.post('/send', function(req, res) {
-    const amount = Number(req.body.amount);
-    const sender = req.body.sender
-
-    client.messages 
-      .create({
-         from: 'whatsapp:+14155238886',
-         body: `${sender} sent you ${amount} beers from a post request`,
-         to: 'whatsapp:+34652568088'
-       });
-    //const response = new MessagingResponse();
-   // response.message(`Beer`)
-    res.status(200).send("beer sent")
-});
-*/
-app.use('/route', superRouter);
  
 module.exports = app;
